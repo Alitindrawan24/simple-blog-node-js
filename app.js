97fc0a95f4c8b58b70f5c94f9577e6bfc7ff119e
Alitindrawan24 <alitindrawan71@gmail.com>
2021-07-30 19:09:07 +0800

    init migration using sequelieze

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const port = 3000;
 
 app.engine('ejs', require('express-ejs-extend'));
 app.use(express.static('public'))
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 app.get('/login', (req, res) => {
     res.render('login.ejs');
+});
+app.post('/register', (req, res) => {
+    console.log(req.body);
 });
 app.get('/register', (req, res) => {
     res.render('register.ejs');
