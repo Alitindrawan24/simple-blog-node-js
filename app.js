4b817b9fea35a9bab395bf9e7947b9161dcab62b
Alitindrawan24 <alitindrawan71@gmail.com>
2021-08-01 09:04:20 +0800

    create user from ligin

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const {
+    body,
+    validationResult
+} = require('express-validator');
 const app = express()
 const port = 3000;
 
+const model = require('./models/index');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 app.engine('ejs', require('express-ejs-extend'));
 app.use(express.static('public'))
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-    extended: true
+app.use(express.urlencoded({
+    extended: false
 }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -17,8 +23,41 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
-app.post('/register', (req, res) => {
-    console.log(req.body);
+app.post('/register', async (req, res) => {
+
+    const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
+    
+    // create user model
+    const user = {
+        fullName: req.body.full_name,
+        email: req.body.email,
+        password:hashPassword
+    };
+    // validate user model
+    const validationResult = body(user);
+    if (validationResult.error) {
+        res.render('register.ejs', {
+            error: validationResult.error
+        });
+    } else {
+        // create user
+        try {
+            const users = await model.User.create(user);
+            if (users) {
+                res.status(201).json({
+                    'status': 'OK',
+                    'messages': 'User berhasil ditambahkan',
+                    'data': users,
+                })
+            }
+        } catch (err) {
+            res.status(400).json({
+                'status': 'ERROR',
+                'messages': err.message,
+                'data': {},
+            })
+        }
+    }
 });
 app.get('/register', (req, res) => {
     res.render('register.ejs');
