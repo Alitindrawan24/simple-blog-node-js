afb5e69c18d4c2be8669c31de3743f2e2fd1ee3f
Alitindrawan24 <alitindrawan71@gmail.com>
2021-08-03 09:46:15 +0800

    register contorller

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,6 +1,48 @@
+const model = require('../models/index');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const {
+    body,
+    validationResult
+} = require('express-validator');
+
 const RegisterController = {
-    index(req, res){
+    index(req, res) {
         res.render('register.ejs');
+    },
+    store(req, res) {
+        const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
+
+        // create user model
+        const user = {
+            fullName: req.body.full_name,
+            email: req.body.email,
+            password: hashPassword
+        };
+        // validate user model
+        const validationResult = body(user);
+        if (validationResult.error) {
+            res.render('register.ejs', {
+                error: validationResult.error
+            });
+        } else {
+            // create user
+            try {
+                const users = model.User.create(user);
+                if (users) {
+                    res.status(201).json({
+                        'status': 'OK',
+                        'messages': 'User berhasil ditambahkan',
+                    })
+                }
+            } catch (err) {
+                res.status(400).json({
+                    'status': 'ERROR',
+                    'messages': err.message,
+                })
+            }
+        }
     }
 }
 
--- routes/web.js
+++ routes/web.js
@@ -2,15 +2,6 @@ const express = require('express')
 const router = express.Router()
 const RegisterController = require('../controllers/RegisterController');
 
-const {
-    body,
-    validationResult
-} = require('express-validator');
-
-const model = require('../models/index');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
 router.get('/', (req, res) => {
     res.render('index.ejs');
 });
@@ -18,42 +9,7 @@ router.get('/login', (req, res) => {
     res.render('login.ejs');
 });
 
-router.post('/register', async (req, res) => {
-
-    const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
-    
-    // create user model
-    const user = {
-        fullName: req.body.full_name,
-        email: req.body.email,
-        password:hashPassword
-    };
-    // validate user model
-    const validationResult = body(user);
-    if (validationResult.error) {
-        res.render('register.ejs', {
-            error: validationResult.error
-        });
-    } else {
-        // create user
-        try {
-            const users = await model.User.create(user);
-            if (users) {
-                res.status(201).json({
-                    'status': 'OK',
-                    'messages': 'User berhasil ditambahkan',
-                    'data': users,
-                })
-            }
-        } catch (err) {
-            res.status(400).json({
-                'status': 'ERROR',
-                'messages': err.message,
-                'data': {},
-            })
-        }
-    }
-});
+router.post('/register', RegisterController.store);
 router.get('/register', RegisterController.index);
 
 module.exports = router
